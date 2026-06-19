22d7cc922ae2eca2cedf79a2a68727a3f3d81783
Leo Sianturi <maringansigalingging7@gmail.com>
2026-06-19 11:42:17 +0700

    feat: customize Announcement and News resources with auto slug and user relation

--- app/Filament/Resources/News/Pages/CreateNews.php
+++ app/Filament/Resources/News/Pages/CreateNews.php
@@ -4,8 +4,19 @@ namespace App\Filament\Resources\News\Pages;
 
 use App\Filament\Resources\News\NewsResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class CreateNews extends CreateRecord
 {
     protected static string $resource = NewsResource::class;
+
+
+protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['slug']     = Str::slug($data['title']) . '-' . time();
+        $data['users_id'] = Auth::id();
+
+        return $data;
+    }
 }
