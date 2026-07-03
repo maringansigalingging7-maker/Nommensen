5f9b0a3f1ed917955ca0531e6e99626d3f908bd8
Leo Sianturi <maringansigalingging7@gmail.com>
2026-07-03 12:06:04 +0700

    feat: implement dynamic frontend pages with CMS data

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/css/**/*.css',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
